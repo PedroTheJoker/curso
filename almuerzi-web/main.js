79bdae6de1a7c1816a59696ec048d983535a66a8
PedroTheJoker <PedroTheJoker@users.noreply.github.com>
2020-10-06 22:27:09 +0200

    avances curso 6/10

--- almuerzi-web/main.js
+++ almuerzi-web/main.js
@@ -1,5 +1,61 @@
+const stringToHTML = (s) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(s, "text/html");
+  return doc.body.firstChild;
+};
+
+const renderItem = (item) => {
+  const element = stringToHTML(`<li data-id="${item._id}">${item.name}</li>`);
+
+  element.addEventListener("click", () => {
+    const mealsList = document.getElementById("meals-list");
+    array
+      .from(mealsList.children)
+      .forEach((x) => x.classList.remove("selected"));
+    element.classList.add("selected");
+    const mealsIdInput = document.getElementById("meals-id");
+    mealsIdInput.value = item._id;
+  });
+
+  return element;
+};
+
 window.onload = () => {
-  fetch("https://serverless.pedrothejoker.vercel.app/api/meals")
+  const orderForm = document.getElementById("order");
+  orderForm.onsubmit = (e) => {
+    e.preventDefault();
+    const mealId = document.getElementById("meals-id");
+    const mealIdValue = mealId.value;
+    if (!mealsIdvalue) {
+      alert("Debes seleccionar un dato");
+      return;
+    }
+
+    const order = {
+      meal_id: mealIdValue,
+      user_id: "Puma",
+    };
+  };
+  fetch(
+    "https://serverless.pedrothejoker.vercel.app/api/meals" /* {
+    method: "GET", // POST, PUT, DELETE
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "content-type": "application/json",
+    },
+    redirect: "follow",
+    body: JSON.stringify({ user: "lala", password: "1234" }),
+   }*/
+  )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      const mealsList = document.getElementById("meals-list");
+      const submit = document.getElementById("submit");
+      const listItems = data.map(renderItem);
+      mealsList.removeChild(mealsList.firstElementChild);
+      listItems.forEach((element) => mealsList.appendChild(element));
+      submit.removeAttribute("disabled");
+    });
 };
